5c55b56f1d08673989b83a7bde8f56c25cc8caab
Evi1 <w1zirn1996@gmail.com>
2017-02-28 15:09:42 +0800

    Openshift surport

--- global/global.go
+++ global/global.go
@@ -16,12 +16,13 @@ func init() {
 	CookieList = make(map[string]*cookiejar.Jar)
 	ErrorList = make(map[string]bool)
 	RunList = make(map[string]map[string]string)
-	BasePath = filepath.ToSlash(os.Getenv("GOPATH")) + "/src/github.com/Evi1/Tieba_Sign-Go---Copy"
 	ip:=os.Getenv("OPENSHIFT_GO_IP")
 	port:=os.Getenv("OPENSHIFT_GO_PORT")
 	if len(ip)>0 && len(port)>0{
 		Server = ip+":"+port
+		BasePath = "../."
 	}else{
 		Server = ":60080"
+		BasePath = filepath.ToSlash(os.Getenv("GOPATH")) + "/src/github.com/Evi1/Tieba_Sign-Go---Copy"
 	}
 }
